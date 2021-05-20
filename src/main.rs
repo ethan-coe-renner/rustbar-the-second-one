6ed063b7be123d712891be5c7980ab0a00c62d27
Ethan Coe-Renner <e.coerenner@gmail.com>
2021-05-19 17:40:13 -0700

    remove news widget, not working with elfeed

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
             rbtso::music(),
             rbtso::updates(),
             rbtso::tasks(),
-            rbtso::news(),
+            // rbtso::news(),
             rbtso::volume(),
             rbtso::battery(),
         ]);
